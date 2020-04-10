e2d28c4bfb68043867100e531851c2cf1dc7210e
Dima <3620849@gmail.com>
2020-04-10 22:51:54 +0300

    fix multi threading issue at user dao service

--- src/main/java/com/weiss/weissdata/config/AppConfig.java
+++ src/main/java/com/weiss/weissdata/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.weiss.weissdata.config;
 
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.internal.MongoClientImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,17 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableMongoRepositories(basePackages = "com.weiss.weissdata.repository")
 @Configuration
-public class AppConfig  {
+public class AppConfig extends AbstractMongoClientConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "test";
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        return MongoClients.create("mongodb://localhost:27017/?replicaSet=rs0&w=majority");
+    }
     @Bean
     MongoTransactionManager transactionManager(MongoDbFactory dbFactory) {
         return new MongoTransactionManager(dbFactory);
--- src/main/java/com/weiss/weissdata/services/UserService.java
+++ src/main/java/com/weiss/weissdata/services/UserService.java
@@ -4,22 +4,24 @@ import com.weiss.weissdata.model.UserInfo;
 import com.weiss.weissdata.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.FileSystemException;
 import java.util.Optional;
 
-@Component
+@Service
 public class UserService {
     @Autowired
     UserRepository repository;
 
-    @Transactional
-    public void addIfUserNotExist(UserInfo userInfo) throws IllegalArgumentException {
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public synchronized void addIfUserNotExist(UserInfo userInfo) throws IllegalArgumentException {
         Optional<UserInfo> byUsername = repository.findByLogin(userInfo.getLogin());
         byUsername.ifPresent(s->{throw new IllegalArgumentException("User with name "+ userInfo.getUsername()+" already exist");});
-        repository.save(userInfo);
+        repository.insert(userInfo);
     }
 
 }
